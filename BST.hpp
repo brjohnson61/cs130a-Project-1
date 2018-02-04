0d269b1ebb451cb7c3fef0a09455e693cc5fd8ff
Blake Johnson <root@Blake-PC.localdomain>
2018-02-03 22:58:28 -0800

    implements BST.hpp

--- BST.hpp
+++ BST.hpp
@@ -10,6 +10,8 @@
 #define BST_hpp
 
 #include <stdio.h>
+#include <string>
+#include "Node.hpp"
 
 #endif /* BST_hpp */
 
@@ -17,9 +19,21 @@ using namespace std;
 
 
 class BST{
+    Node* root;
+    int height;
+    int size;
+
 public:
-    
-private:
+    BST(BST* other);
+    BST();
+    BST(Node* root);
+
+    bool search(string word);
+    void insert(string word);
+    void remove(string word);
+    void sort(void);
+    void rangeSearch(string startWord, string endWord);
+
     
     
 };
